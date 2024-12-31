e699f8c640dfbd60916deea7c87af484646abf94
anrame <anitha.ramesh@gmail.com>
2024-12-31 15:36:49 +0530

    Calc.java

---
+++ Calc.java
@@ -0,0 +1,10 @@
+class Calc
+{
+public static void main(String args[])
+{
+System.out.println("3+3="+(3+3));
+System.out.println("3-3="+(3-3));
+System.out.println("3*3="+(3*3));
+System.out.println("3/3="+(3/3));
+}
+}
